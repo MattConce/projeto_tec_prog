c3afba579d6e397d47ea490ba258e647614145e4
MattConce <mattsantos.conce@gmail.com>
2018-06-08 20:18:59 -0300

    app com login, sessão e logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
+      flash[:success] = "Bem vindo ao Apoio Combate ao Câncer"
       redirect_to user_url(@user)
     else
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   attr_accessor :password, :password_confirmation
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
   validates :name, presence:true, length: {maximum:50}
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, length: {maximum:255},
                     format: {with: VALID_EMAIL},
                     uniqueness: { case_sensitive: false }
-  #has_secure_password
+  has_secure_password
   validates :password, presence:true, length: {minimum: 6}
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
