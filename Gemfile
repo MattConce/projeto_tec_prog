1822a45b0570d527778394966fc03fd00af046c9
Marcelo Schmitt <marcelo.schmitt1@gmail.com>
2018-06-09 22:39:41 -0300

    Testes básicos do Usuário com RSpec
    Testes básicos da entidade User com RSpec e FactoryBot. Para rodar os testes execute 'bundle exec rspec'.

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  # Para os testes
+  gem 'rspec-rails', '~> 3.6'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -57,6 +60,10 @@ gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.11'
 
 gem "pg"
+
+# Para limpar o banco de dados de teste depois de cada teste do RSpec
+gem 'database_cleaner', group: :test
+
 #gem "haml"
 #gem "haml-rails", :group => 'development'
 
---
+++ spec/user_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  
+  before(:all) do
+    @user1 = create(:user)
+    # @user1 = build_stubbed(:user)
+    # @user1 = build(:user)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user1).to be_valid
+  end
+  
+  it "is not valid without a password" do 
+    user2 = build(:user, password: nil)
+    expect(user2).to_not be_valid
+  end
+  
+  it "is not valid without a username" do 
+    user2 = build(:user, name: nil)
+    expect(user2).to_not be_valid
+  end
+
+  it "has a unique username" do
+    user2 = build(:user, name: @user1.name)
+    expect(user2).to_not be_valid
+  end
+
+  it "has a unique email" do
+    user2 = build(:user, email: @user1.email)
+    expect(user2).to_not be_valid
+  end
+  
+  it "is not valid without an email" do
+    user2 = build(:user, email: nil)
+    expect(user2).to_not be_valid
+  end
+
+end
