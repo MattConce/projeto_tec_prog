8f02c82fcccb3aad4442ac319d4b0fdf2a698560
MattConce <mattsantos.conce@gmail.com>
2018-06-26 18:07:39 -0300

    enviando emails, sucessogit add -A

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,18 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'apoiocombateaocancer@gmail.com',
+    :password             => 'Comb123456',
+    :authentication       => "login",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 end
